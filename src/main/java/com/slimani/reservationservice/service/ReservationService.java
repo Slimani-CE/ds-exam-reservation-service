6ae33e21845e7c02122dafb48b3bef10d3fb4cea
Mustapha SLIMANI <itsmustaphahere@gmail.com>
2024-01-01 14:19:01 +0100

    update

--- src/main/java/com/slimani/reservationservice/service/ReservationService.java
+++ src/main/java/com/slimani/reservationservice/service/ReservationService.java
@@ -2,7 +2,6 @@ package com.slimani.reservationservice.service;
 
 import com.slimani.reservationservice.dto.ReservationRequestDTO;
 import com.slimani.reservationservice.dto.ReservationResponseDTO;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
